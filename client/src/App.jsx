b26899631146ab3a4e2f6c3589509d0e0f94d598
Ramon Alvarez <ralv212@wgu.edu>
2024-07-12 10:13:52 -0400

    Add react-router-dom and empty pages
    - Added React Router Dom to route to initial demo pages: Home, About, Recipes, and NoPage using a Layout component as a layout.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/layout/Layout";
+import Home from "./pages/home/HomePage";
+import Recipes from "./pages/recipes/RecipesPage";
+import About from "./pages/about/AboutPage";
+import NoPage from "./pages/nopage/NoPage";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="recipes" element={<Recipes />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
