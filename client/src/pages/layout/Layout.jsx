a3b5efc55fc4d1d19edaa5d9bce60ebbe9ec0478
Ramon Alvarez <ralv212@wgu.edu>
2024-07-12 10:31:55 -0400

    Finalize Layout and add react icons
    - Removed Header component and moved it to layout.
    - Added a footer and body (outlet) to the layout.
    - Added React Icons for Github Icon.

--- client/src/pages/layout/Layout.jsx
+++ client/src/pages/layout/Layout.jsx
@@ -1,8 +1,28 @@
-import Header from "./components/Header";
+import { Outlet, Link } from "react-router-dom";
+import { FaGithub } from 'react-icons/fa';
 
 function Layout(){
     return(
-        <Header />
+        <div className="flex flex-col min-h-screen">
+            <nav className="flex flex-row justify-between text-xl border-b-2 p-4 items-center">
+                <Link to="/recipes">All Recipes</Link>
+
+                <Link to="/" className="text-5xl">Recipe Book</Link>
+
+                <Link to="/about">About</Link>
+            </nav>
+
+            <div className="flex-grow">
+                <Outlet />
+            </div>
+
+            <footer className='flex border-t-2 justify-end content-center items-center py-2 text-center'>
+                <p className='m-0'>&copy; {new Date().getFullYear()} - Ramon Alvarez</p>
+                <a href="https://github.com/HeftyTie/RecipeBook" target="_blank">
+                    <FaGithub className="mx-2 text-xl" />
+                </a>
+            </footer>
+        </div>
     );
 }
 
